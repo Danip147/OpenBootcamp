6c66bf26de45a1089c55cc4c828158eda85c5578
Dani <danielporras147@outlook.es>
2023-02-10 15:35:05 +0100

    Sobrecarga de funciones

---
+++ Java/java_openBootcamp/src/tiposDeDatos/SobrecargaFunciones.java
@@ -0,0 +1,29 @@
+package tiposDeDatos;
+
+public class SobrecargaFunciones {
+
+	// Sobrecargar es cuando la funcion tiene el mismo nombre pero el tipo de dato que le entra
+	// (parametros) es distinto.
+	
+	
+	public static int suma(int n1, int n2) {
+		return n1 + n2; 
+	}
+	
+	public static double suma(double n1, double n2) {
+		return n1 + n2; 
+	}
+	
+	
+
+	
+	public static void main(String[] args) {
+		int suma1 = suma(2,2);
+		System.out.println(suma1);
+		
+		double suma2 = suma(2.4, 4.2);
+		System.out.println(suma2);
+		
+	}
+
+}
