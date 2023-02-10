f54fca774b28ab938960b80e2b2fdfe8b8da8d03
Dani <danielporras147@outlook.es>
2023-02-10 15:34:52 +0100

    Prueba funciones

---
+++ Java/java_openBootcamp/src/tiposDeDatos/PruebaFunciones.java
@@ -0,0 +1,13 @@
+package tiposDeDatos;
+
+public class PruebaFunciones {
+
+	public static void main(String[] args) {
+		Funciones.holaMundo();            // En este caso SI me deja acceder porque la visualizacion es PUBLIC
+//		Funciones.devolverHola();         // En este caso NO me deja acceder porque la visualizacion es PRIVATE
+		Funciones.holaMundo();        	  // En este caso SI que puedo accder porque la visualizacion es PROTECTED
+										  // si está en el mismo paquete si que puede acceder a ella.
+		
+	}
+
+}
