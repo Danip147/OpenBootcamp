7fdc04d599a1b472d025b710109516352d1fbed4
Dani <danielporras147@outlook.es>
2023-02-10 15:35:14 +0100

    tipos de datos

--- Java/java_openBootcamp/src/tiposDeDatos/TiposDatos.java
+++ Java/java_openBootcamp/src/tiposDeDatos/TiposDatos.java
@@ -3,8 +3,35 @@ package tiposDeDatos;
 public class TiposDatos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		          
 
+		// enteros 
+		byte number1 = 1;  // 1 byte
+		short number2 = 2; // 2 byte 
+		int number3 = 3;   // 3 byte
+		long number4 = 4;  // 8 byte
+		
+		// punto flotante
+		float decimal = 4.9f;
+		double decimal2 = 4.9;
+		
+		// caracter
+		char caracter1 = 'a';
+		
+		//boolean
+		boolean verdadero = true;
+		boolean falso = false;
+		
+		// cadenas de texto
+		String nombre = "Daniel";
+		String apellido = "Porras";
+		
+		// tipos envoltorio
+		Integer numero = null;
+		Long numero2 = 2L;
+		
+		
 	}
+	
 
 }
