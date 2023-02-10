78c87f07882fab11a7ec0b0f0998fd598dc1eb11
Dani <danielporras147@outlook.es>
2023-02-10 16:00:10 +0100

    ejercicio01

--- Java/ejercicios_Java_OpenBootcamp/src/ejercicio01/Ejercicio01.java
+++ Java/ejercicios_Java_OpenBootcamp/src/ejercicio01/Ejercicio01.java
@@ -3,7 +3,52 @@ package ejercicio01;
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		byte number1 = 1;  // 1 byte
+		short number2 = 2; // 2 byte 
+		int number3 = 3;   // 3 byte
+		long number4 = 4;  // 8 byte
+		
+		// punto flotante
+		float decimal = 4.9f;
+		double decimal2 = 4.9;
+		
+		// caracter
+		char caracter1 = 'a';
+		
+		//boolean
+		boolean verdadero = true;
+		boolean falso = false;
+		
+		// cadenas de texto
+		String nombre = "Daniel";
+		String apellido = "Porras";
+		
+		// tipos envoltorio
+		Integer numero = null;
+		Long numero2 = 2L;
+		
+		
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(number3);
+		System.out.println(number4);
+		
+		System.out.println(decimal);
+		System.out.println(decimal2);
+		
+		System.out.println(caracter1);
+		
+		System.out.println(verdadero);
+		System.out.println(falso);
+		
+		System.out.println(nombre);
+		
+		System.out.println(numero);
+		System.out.println(numero2);
+		
+		
+		
 
 	}
 
