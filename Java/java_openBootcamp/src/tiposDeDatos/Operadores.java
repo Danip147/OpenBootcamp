dc0fad03ec7091708617cef07f4d7873eaff63c9
Dani <danielporras147@outlook.es>
2023-02-10 15:05:12 +0100

    operadores

--- Java/java_openBootcamp/src/tiposDeDatos/Operadores.java
+++ Java/java_openBootcamp/src/tiposDeDatos/Operadores.java
@@ -3,8 +3,54 @@ package tiposDeDatos;
 public class Operadores {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// Operadores
+		
+		// aritmeticos
+		
+		// + - / * %
+		
+		int number1 = 1 ;
+		int number2 = 2;
+		
+		int resultado = number1 + number2; 
+		int resultado2 = number1 - number2;
+		int resultado3 = number1 * number2;
+		int resultado4 = number1 / number2;
+		
+		// logicos, relacion, comparacion, booleanos
+		
+		/*
+		  >
+		  >=
+		  <
+		  =<
+		  ==
+		  !=
+		  && and
+		  || or
+		  ! negacion
+		 */
+		
+		 // asignacion
+		 /*
+		  =
+		  +=
+		  -=
+		  /=
+		  *=
+		  %=
+		  */
+		
+		  // incremento
+		  // ++
+		
+		  // decremento
+		  // --
+		 
+		  // concatenacion
+		  // +
+		
 	}
 
 }
