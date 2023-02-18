591b80307c0aa97cf06923dea1ecb3c0576edef2
Dani <danielporras147@outlook.es>
2023-02-18 20:05:40 +0100

    scanner

--- Java/java_openBootcamp/src/teoriaJava/ScannerMain.java
+++ Java/java_openBootcamp/src/teoriaJava/ScannerMain.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
 public class ScannerMain {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Por favor introduce un nombre: ");
 		String nombre = sc.nextLine();
-		
+
 		System.out.println("Por favor introduce un número: ");
 		int numero = sc.nextInt();
-		
+
 		System.out.println("El nombre introducido es: " + nombre);
 		System.out.println("El número introducido es: " + numero);
-		 
-		
+
 		sc.close();
 
 	}
